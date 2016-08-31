5b5c38567095ebad7326f613e94f1a87169461b9
Mufid <mufid@outlook.com>
2016-08-31 20:35:06 +0700

    Allow non-alphanumeric character in db name and user name

--- lib/sql/postgres_head.sql
+++ lib/sql/postgres_head.sql
@@ -1,8 +1,8 @@
-DROP DATABASE s{db_name};
-CREATE DATABASE s{db_name};
-CREATE USER s{user_name} WITH PASSWORD 'password';
-ALTER USER s{user_name} with createdb;
-GRANT ALL PRIVILEGES ON DATABASE s{db_name} TO s{user_name};
+DROP DATABASE "s{db_name}";
+CREATE DATABASE "s{db_name}";
+CREATE USER "s{user_name}" WITH PASSWORD 'password';
+ALTER USER "s{user_name}" with createdb;
+GRANT ALL PRIVILEGES ON DATABASE "s{db_name}" TO "s{user_name}";
 ALTER DATABASE "s{db_name}" OWNER TO "s{user_name}";
 
 \c s{db_name};
